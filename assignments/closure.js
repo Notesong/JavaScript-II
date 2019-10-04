dcfb2bdbd4c57b56e40071aa16bbeb91e1050a2f
Notesong <notesong.git@outlook.com>
2019-10-04 16:56:21 -0400

    Add assignment comments back into closure.js.

--- assignments/closure.js
+++ assignments/closure.js
@@ -13,16 +13,27 @@ youCalled();
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 2: Implement a "counter maker" function ====
-const counterMaker = (limit) => {
+
+const counterMaker = (limit) => {  // added limit parameter for Challenge 3
+
+  // IMPLEMENTATION OF counterMaker:
+  // 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
+  // 2- Declare a function `counter`. It should increment and return `count`.
+  //      NOTE: This `counter` function, being nested inside `counterMaker`,
+  //      "closes over" the `count` variable. It can "see" it in the parent scope!
+  // 3- Return the `counter` function.
+  
   let count = 0;
   return function counter() {
     ++count;
+    // added if statement for Challenge 3
     if (count > limit) {
       count = 1;
     }
     return count;
   };
 };
+
 // ==== Challenge 3: Make `counterMaker` more sophisticated ====
 // It should have a `limit` parameter. Any counters we make with `counterMaker` will refuse to go over the limit, and start back at 1.
 let counted = counterMaker(3);
